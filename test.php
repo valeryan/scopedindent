3853a92fce67afeff3b79795bb7141e2db4deaf5
Samuel Hilson <samrhilson@gmail.com>
2019-02-01 09:11:06 -0600

    initial commit

---
+++ test.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Created by .
+ *
+ * Copyright (C) 2018 .
+ */
+class TestClass
+{
+	public function functionName()
+	{
+		if (true) {
+			$test = [
+				'name' => 'aa',
+				'icon' => 'fa-wrench',
+				'stuff' => ['internal', 'array'],
+				['array']
+			];
+		}
+		return $test;
+	}
+}
