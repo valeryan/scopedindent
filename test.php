92bcb01af1d625a7126c870e2abb2de3bf10b30d
Samuel Hilson <samrhilson@gmail.com>
2019-02-01 11:11:27 -0600

    update code to better represent issue

--- test.php
+++ test.php
@@ -9,14 +9,37 @@ class TestClass
 {
 	public function functionName()
 	{
+		define('NS_MAIN', 'main');
+		$pos = 0;
+		$tag = 0;
+		// Array indent detected incorrectly
 		if (true) {
 			$test = [
 				'name' => 'aa',
 				'icon' => 'fa-wrench',
 				'stuff' => ['internal', 'array'],
-				['array']
+				'array',
+				($pos + 1),
+				$tag,
+				1,
+				true,
+				NS_MAIN
 			];
 		}
+		// Chained indent detected incorrectly
+		$this->bar()
+			->baz();
+
 		return $test;
+	}
+
+	private function bar()
+	{
+		return $this;
+	}
+
+	private function baz()
+	{
+		return $this;
 	}
 }
